e5167f71b507eedd8f76322cdb7f0da2b9c6c7c7
Anton Kozlov <akozlov@azul.com>
2021-02-25 23:24:59 +0300

    Add bad examples

--- llvm/example.c
+++ llvm/example.c
@@ -8,3 +8,43 @@ int main(){
 	}
 	return 0;
 }
+
+static void fn() {
+	while(1);
+}
+
+int testfn_fail() {
+	besc_tracepoint("tp1");
+	fn();
+	besc_tracepoint("tp2");
+	return 1;
+}
+
+int test1_fail() {
+	besc_tracepoint("tp1");
+	if (rand()) {
+		besc_tracepoint("tp2");
+	}
+}
+
+int test2_fail() {
+	besc_tracepoint("tp1");
+	if (rand()) {
+		while(1);
+	}
+	besc_tracepoint("tp2");
+}
+
+int test3_fail() {
+	besc_tracepoint("tp1");
+	while(1);
+	besc_tracepoint("tp2");
+}
+
+int test4_fail() {
+	besc_tracepoint("tp1");
+	if (0) {
+		while(1);
+	}
+	besc_tracepoint("tp2");
+}
