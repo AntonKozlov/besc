ceac6206abe624b3ceaad1842fe160fc71b77212
Oleg Smirnov <sm-oleg1209@yandex.ru>
2021-02-28 17:56:46 +0700

    Add dependency to resolve errs() Remove *.ll Fix Makefile and .gitignore

--- llvm/check_cycles.cpp
+++ llvm/check_cycles.cpp
@@ -1,6 +1,7 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IRReader/IRReader.h"
 #include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/InstVisitor.h"
 #include "llvm/IR/BasicBlock.h"
 
