7b52fcc5c0e6e3a0c461664277f842585bdc8850
Nikita Vlaev <nikitavlaev00@gmail.com>
2021-04-01 01:15:28 +0300

    Refactor check cycles original algo

--- llvm/check_cycles.cpp
+++ llvm/check_cycles.cpp
@@ -13,187 +13,301 @@ using namespace llvm;
 using namespace std;
 
 typedef string TracePoint;
+typedef unsigned Vertex;
+typedef vector<vector<Vertex>> Graph;
 
-string name_fun_tp = "besc_tracepoint_";
-int name_fun_tp_len = name_fun_tp.length();
+// status of trace searching
+class SearchingState
+{
+public:
+    bool StartTPNotFound;       // can't find function "{name_fun_tp}{start_tp}"
+    bool FinalTPNotFound;       // can't find function "{name_fun_tp}{final_tp}"
+    bool FinalTPUnreachable;    // there isn't a path from start_tp to final_tp
+    bool LoopFound;             // there is loop in path between start_tp and final_tp
+    bool FinalTPAvoidable;      // there is path from start_tp, but doesn't reach final_tp
 
-enum class SearchingState {
-    StartTPNotFound,
-    FinalTPNotFound,
-    CantReach,
-    LoopFound,
-    MaybeCantReach,
-    Success
-};
+    SearchingState() : StartTPNotFound(false),
+                       FinalTPNotFound(false),
+                       FinalTPUnreachable(false),
+                       LoopFound(false),
+                       FinalTPAvoidable(false) {}
 
-struct DfsStatus {
-    bool reached_final_tp = false;
-    bool always_reached_final_tp = true;
-    bool loop_found = false;
-    bool visited = false;
-    bool not_visited = true;
-};
-
-TracePoint start_tp, final_tp;
-BasicBlock *start_bb, *final_bb;
-map<BasicBlock *, vector < BasicBlock * >> graph;
-map<TracePoint, BasicBlock * > block_with_tp;
-map<BasicBlock *, DfsStatus> status;
-map<BasicBlock *, int> ind;
-int amt_inds = 0;
-vector<BasicBlock * > dfs_stack;
-
-ostream& operator<<(ostream& out, const SearchingState state){
-    string str;
-    switch(state) {
-        case SearchingState::StartTPNotFound:
-            str = "Tracepoint \"" + start_tp + "\" was not found"; break;
-        case SearchingState::FinalTPNotFound:
-            str = "Tracepoint \"" + final_tp + "\" was not found"; break;
-        case SearchingState::CantReach:
-            str = "There isn't trace between tracepoint \"" + start_tp + "\" and tracepoint \"" + final_tp + "\""; break;
-        case SearchingState::LoopFound:
-            str = "There is loop in trace between tracepoint \"" + start_tp + "\" and tracepoint \"" + final_tp + "\""; break;
-        case SearchingState::MaybeCantReach:
-            str = "There is trace which starts from tracepoint \"" + start_tp + "\", but doesn't go through tracepoint \"" + final_tp + "\""; break;
-        case SearchingState::Success:
-            str = "OK"; break;
+    int to_int()
+    {
+        return StartTPNotFound * 16 +
+               FinalTPNotFound * 8 +
+               FinalTPUnreachable * 4 +
+               LoopFound * 2 +
+               FinalTPAvoidable;
     }
+};
+
+template <class T1, class T2, class T3>
+map<T1, T3> mapUnion(map<T1, T2> &map_1, map<T2, T3> &map_2)
+{
+    map<T1, T3> res_map;
+    for (auto [key_1, key_2] : map_1)
+    {
+        if (map_2.find(key_2) != map_2.end())
+        {
+            res_map[key_1] = map_2[key_2];
+        }
+    }
+    return res_map;
+}
+
+// create graph of Module
+class GraphCreator : public InstVisitor<GraphCreator>
+{
+
+private:
+    Graph graph;
+    map<BasicBlock *, Vertex> blockIdx;
+    unsigned amtBlocks;
+
+public:
+    GraphCreator() : amtBlocks(0) {}
+
+    Graph create(Module &M)
+    {
+        visit(M);
+        return graph;
+    }
+
+    map<BasicBlock *, Vertex> getBlockIdx() { return blockIdx; }
+
+    void visitBranchInst(BranchInst &BI)
+    {
+        BasicBlock *from = BI.getParent();
+        for (BasicBlock *to : BI.successors())
+        {
+            for (BasicBlock *bb : {from, to})
+            {
+                // if bb is not found in blockIdx
+                if (blockIdx.find(bb) == blockIdx.end())
+                {
+                    blockIdx[bb] = amtBlocks;
+                    amtBlocks++;
+                    graph.push_back({});
+                }
+            }
+            graph[blockIdx[from]].push_back(blockIdx[to]);
+        }
+    }
+};
+
+// find tracepoints in Module
+class TracePointFinder : public InstVisitor<TracePointFinder>
+{
+
+private:
+    map<TracePoint, BasicBlock *> tracepoints;
+    inline static string tp_prefix = "besc_tracepoint_";
+
+public:
+    TracePointFinder() {}
+
+    map<TracePoint, Vertex> find(Module &M, map<BasicBlock *, Vertex> &blockIdx)
+    {
+        visit(M);
+        return mapUnion(tracepoints, blockIdx);
+    }
+
+    void visitCallInst(CallInst &CI)
+    {
+        BasicBlock *curBlock = CI.getParent();
+        string name_fun = CI.getCalledFunction()->getName().str();
+        if (name_fun.substr(0, tp_prefix.length()) == tp_prefix)
+        {
+            TracePoint tp = name_fun.substr(tp_prefix.length());
+            tracepoints[tp] = curBlock;
+        }
+    }
+};
+
+// TODO: add priority output
+// pretty print of SearchingState
+ostream &operator<<(ostream &out, const SearchingState state)
+{
+    string str = "";
+    str += "Start tracepoint was not found : " + (string)(state.StartTPNotFound ? "true" : "false") + "\n";
+    str += "Final tracepoint was not found : " + (string)(state.FinalTPNotFound ? "true" : "false") + "\n";
+    str += "There is no path between start tracepoint and final tracepoint : " + (string)(state.FinalTPUnreachable ? "true" : "false") + "\n";
+    str += "There is a loop in trace between start tracepoint and final tracepoint : " + (string)(state.LoopFound ? "true" : "false") + "\n";
+    str += "There is a path from start tracepoint, that doesn't reach final tracepoint : " + (string)(state.FinalTPAvoidable ? "true" : "false") + "\n";
     return out << str;
 }
 
-void dfs(BasicBlock *bb) {
-    if (bb == final_bb) {
-        //cout << "! " << ind[bb] << endl;
-        status[bb].reached_final_tp = true;
-        status[bb].visited = true;
-        return;
-    }
-    //cout << "> " << ind[bb] << endl;
-    status[bb].not_visited = false;
-    dfs_stack.push_back(bb);
-    for (auto to : graph[bb]) {
-        //cout << "? " << ind[to] << " <- " << ind[bb] << endl;
-        if (status[to].not_visited) {
-            //cout << "v" << endl;
-            dfs(to);
-        } else if ( ! status[to].visited) {
-            //cout << "c (";
-            for (auto w = dfs_stack.rbegin(); *w != to; w++) {
-                status[*w].loop_found = true;
-                //cout << ind[*w] << ", ";
-            }
-            status[to].loop_found = true;
-            //cout << ind[to] << ")" << endl;
-        }
-        if (status[to].visited) {
-            if (status[to].reached_final_tp) {
-                status[bb].loop_found |= status[to].loop_found;
-                status[bb].reached_final_tp = true;
-                status[bb].always_reached_final_tp &= status[to].always_reached_final_tp;
-            } else {
-                status[bb].always_reached_final_tp = false;
-            }
-        }
-    }
-    dfs_stack.pop_back();
-    status[bb].visited = true;
-    //cout << "< " << ind[bb] << " (";
-    //if (status[bb].loop_found) //cout << "loop_found, ";
-    //if (status[bb].reached_final_tp) //cout << "reached_final_tp, ";
-    //if (status[bb].always_reached_final_tp) //cout << "always_reached_final_tp, ";
-    //cout << ")" << endl;
-}
+// find loops in graph
+class LoopsFinder
+{
 
-SearchingState findLoopInTrace() {
-    if (block_with_tp.find(start_tp) == block_with_tp.end())
-        return SearchingState::StartTPNotFound;
-    start_bb = block_with_tp[start_tp];
+private:
+    enum Color
+    {
+        White,
+        Grey,
+        Black,
+    };
 
-    if (block_with_tp.find(final_tp) == block_with_tp.end())
-        return SearchingState::FinalTPNotFound;
-    final_bb = block_with_tp[final_tp];
-    
-    for (auto [block, _] : graph)
-        status[block] = DfsStatus();
-    dfs_stack = {};
+    struct DfsStatus
+    {
+        bool reached_final_tp = false;
+        bool loop_found = false;
+        Color color = White;
+    };
+    Graph graph;
+    vector<DfsStatus> status;
+    vector<Vertex> dfs_stack;
+    map<TracePoint, Vertex> labels;
 
-    dfs(start_bb);
-    
-    if ( ! status[start_bb].reached_final_tp)
-        return SearchingState::CantReach;
-    if (status[start_bb].loop_found)
-        return SearchingState::LoopFound;
-    if ( ! status[start_bb].always_reached_final_tp)
-        return SearchingState::MaybeCantReach;
-    return SearchingState::Success;
-}
-
-struct BranchInstVisitor : public InstVisitor<BranchInstVisitor> {
-    unsigned count;
-
-    BranchInstVisitor() : count(0) {}
-
-    void visitBranchInst(BranchInst &BI) {
-        count++;
-        //cout << "BranchInst found: " << count << "\n";
-        for (unsigned i = 0; i < BI.getNumSuccessors(); i++) {
-            BasicBlock *from = BI.getParent(), *to = BI.getSuccessor(i);
-            if (graph.find(from) == graph.end()) {
-                vector < BasicBlock * > adj = {to};
-                graph.insert(make_pair(from, adj));
-                ind[from] = ++amt_inds;
-            } else {
-                graph[from].push_back(to);
-            }
-            if (graph.find(to) == graph.end()) {
-                vector < BasicBlock * > adj = {};
-                graph.insert(make_pair(to, adj));
-                ind[to] = ++amt_inds;
-            }
-            //cout << ind[from] << " -> " << ind[to] << endl;
-        }
+public:
+    LoopsFinder(Graph &graph_, map<TracePoint, Vertex> &labels_)
+    {
+        graph = graph_;
+        labels = labels_;
+        status.assign(graph.size(), DfsStatus());
     }
 
-    void visitCallInst(CallInst &CI) {
-        BasicBlock *curBlock = CI.getParent();
-        string name_fun = CI.getCalledFunction()->getName().str();
-        if (name_fun.substr(0, name_fun_tp_len) == name_fun_tp) {
-            TracePoint tp = name_fun.substr(name_fun_tp_len);
-            block_with_tp[tp] = curBlock;
+    pair<bool, bool> find(TracePoint start_tp, TracePoint final_tp)
+    {
+        dfs(labels[start_tp], labels[final_tp]);
+        return {status[labels[start_tp]].loop_found,
+                status[labels[start_tp]].reached_final_tp};
+    }
+
+private:
+    void dfs(Vertex v, Vertex final_v)
+    {
+        if (v == final_v)
+        {
+            status[v].reached_final_tp = true;
+            status[v].color = Black;
+            return;
         }
+
+        status[v].color = Grey;
+        dfs_stack.push_back(v);
+        for (auto to : graph[v])
+        {
+            if (status[to].color == White)
+            {
+                dfs(to, final_v);
+            }
+            
+            if (status[to].color == Grey)
+            {
+                for (auto w = dfs_stack.rbegin(); *w != to; w++)
+                {
+                    status[*w].loop_found = true;
+                }
+                status[to].loop_found = true;
+            }
+
+            if (status[to].color == Black)
+            {
+                if (status[to].reached_final_tp)
+                {
+                    status[v].loop_found |= status[to].loop_found;
+                    status[v].reached_final_tp = true;
+                }
+            }
+        }
+        dfs_stack.pop_back();
+        status[v].color = Black;
     }
 };
 
-int main(int argc, char **argv) {
-    if (argc != 4) {
+// find path from start_tp which doesn't reach final_tp
+class FinalTPAvoidableChecker
+{
+
+private:
+    Graph graph;
+    unsigned N;
+    map<TracePoint, Vertex> labels;
+    vector<bool> used;
+
+public:
+    FinalTPAvoidableChecker(Graph &graph_, map<TracePoint, Vertex> &labels_)
+    {
+        graph = graph_;
+        N = graph.size();
+        labels = labels_;
+    }
+
+    bool check(TracePoint start_tp, TracePoint final_tp)
+    {
+        used.assign(N, false);
+        return dfs(labels[start_tp], labels[final_tp]);
+    }
+
+private:
+    bool dfs(Vertex v, Vertex final_v)
+    {
+        used[v] = true;
+        if (v == final_v)
+            return false;
+        if (graph[v].empty())
+            return true;
+        for (Vertex to : graph[v])
+        {
+            if (!used[to] and dfs(to, final_v))
+            {
+                    return true;
+            }
+        }
+        return false;
+    }
+};
+
+// main function of searching loop in trace between start_tp and final_tp
+SearchingState *runSearch(Module &M, TracePoint start_tp, TracePoint final_tp)
+{
+    SearchingState *state = new SearchingState();
+    auto GC = GraphCreator();
+    Graph graph = GC.create(M);
+
+    auto blockIdx = GC.getBlockIdx();
+    auto labels = TracePointFinder().find(M, blockIdx);
+
+    state->StartTPNotFound = labels.find(start_tp) == labels.end();
+    state->FinalTPNotFound = labels.find(final_tp) == labels.end();
+
+    // LoopsFinder still has to collect info about final tp reachability,
+    // so we reuse it as a side effect.
+    auto result = LoopsFinder(graph, labels).find(start_tp, final_tp);
+    state->LoopFound = result.first;
+    state->FinalTPUnreachable = !result.second;
+
+    state->FinalTPAvoidable = FinalTPAvoidableChecker(graph, labels).check(start_tp, final_tp);
+    return state;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+    {
         cerr << "Usage: " << argv[0] << " <IR file> <Start tracepoint> <Final tracepoint>\n";
         return 1;
     }
 
     // Define start and final tracepoints
-    start_tp = TracePoint(argv[2]);
-    final_tp = TracePoint(argv[3]);
+    TracePoint start_tp = TracePoint(argv[2]);
+    TracePoint final_tp = TracePoint(argv[3]);
 
     // Parse the input LLVM IR file into a module.
     SMDiagnostic Err;
     LLVMContext Context;
-    unique_ptr <Module> Mod(parseIRFile(argv[1], Err, Context));
-    if (!Mod) {
+    unique_ptr<Module> Mod(parseIRFile(argv[1], Err, Context));
+    if (!Mod)
+    {
         Err.print(argv[0], errs());
         return 1;
     }
 
-    // Visit all the branch instances and build an oriented graph
-    BranchInstVisitor BIV;
-    BIV.visit(*Mod);
-
-    //for (auto [tp, bb] : block_with_tp) {
-        //cout << "Tracepoint: \"" << tp << "\" in " << ind[bb] << endl;
-    //}
-
     // Run searching of loop in trace
-    cout << findLoopInTrace() << endl;
-
-    return 0;
+    SearchingState *ret = runSearch(*Mod, start_tp, final_tp);
+    cout << (*ret) << endl;
+    return ret->to_int();
 }
