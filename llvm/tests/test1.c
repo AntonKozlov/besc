65acb2d961f1ba812986c7d7b774d1a4db6515c3
Oleg Smirnov <sm-oleg1209@yandex.ru>
2021-04-08 00:16:18 +0300

    Fix test

--- llvm/tests/test1.c
+++ llvm/tests/test1.c
@@ -4,7 +4,7 @@
 int main() {
     besc_tracepoint("main_entry");
     if (rand()) {
-        for (int i = 0; i < 50000; ++i) {
+        for (int i = 0; i < 5; ++i) {
             besc_tracepoint("3");
         }
     } else {
